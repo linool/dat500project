79c8f3d48ecb42a137558aa09b8eece73eac9167
Lingyuan Li <li.lingyuan@gmail.com>
2022-04-28 19:51:06 +0000

    shorten variable names in build_a_language_model_using_3-grams.py

--- build_a_language_model_using_3-grams.py
+++ build_a_language_model_using_3-grams.py
@@ -3,19 +3,19 @@ import sys
 
 input_file = 'sorted_converted_3-grams_vg_aftenposten_output.txt'
 
-def get_first2words_3rdword_count(line):
+def get_words_count(line):
     words = line.split()
     if len(words) != 4:
         return "error","error",1
     for i in range(len(words)-1):
-        words[i]=remove_leading_and_trailing_punctuation(words[i])
+        words[i]=remove_lt_punctuation(words[i])
     try:
         count = int(words[3])
     except:
         count = 1
     return words[0]+' '+words[1], words[2], count
 
-def remove_leading_and_trailing_punctuation(word):
+def remove_lt_punctuation(word):
     n = len(word)
     for i in range(n):
         if word[i] not in string.punctuation:
@@ -31,31 +31,31 @@ def remove_leading_and_trailing_punctuation(word):
             return ''
     return word 
 
-dict_first2words_total_counts = {}
-dict_first2words_3rdword_counts = {}
+dict_total_counts = {}
+dict_counts = {}
 with open(input_file, encoding='utf-8') as f:
     for line in f:
-        first2words, thirdword, count = get_first2words_3rdword_count(line)
-        if first2words not in dict_first2words_total_counts:
-            dict_first2words_total_counts[first2words] = count
-            dict_first2words_3rdword_counts[first2words] = {}
-            dict_first2words_3rdword_counts[first2words][thirdword] = count
+        first2words, thirdword, count = get_words_count(line)
+        if first2words not in dict_total_counts:
+            dict_total_counts[first2words] = count
+            dict_counts[first2words] = {}
+            dict_counts[first2words][thirdword] = count
         else:
-            dict_first2words_total_counts[first2words] = dict_first2words_total_counts[first2words] + count
-            if thirdword not in dict_first2words_3rdword_counts[first2words]:
-                if len(dict_first2words_3rdword_counts[first2words]) < 3:
-                    dict_first2words_3rdword_counts[first2words][thirdword] = count
+            dict_total_counts[first2words] = dict_total_counts[first2words] + count
+            if thirdword not in dict_counts[first2words]:
+                if len(dict_counts[first2words]) < 3:
+                    dict_counts[first2words][thirdword] = count
             else: 
-                dict_first2words_3rdword_counts[first2words][thirdword] = dict_first2words_3rdword_counts[first2words][thirdword]+count
+                dict_counts[first2words][thirdword] = dict_counts[first2words][thirdword]+count
 
-for key in dict_first2words_3rdword_counts:
-    for k in dict_first2words_3rdword_counts[key]:
-        dict_first2words_3rdword_counts[key][k] = dict_first2words_3rdword_counts[key][k]/dict_first2words_total_counts[key]
+for key in dict_counts:
+    for k in dict_counts[key]:
+        dict_counts[key][k] = dict_counts[key][k]/dict_counts[key]
 
 with open("language_model_from_trigrams.txt", "w") as w:
-    for key in dict_first2words_3rdword_counts:
+    for key in dict_counts:
         try:
-            w.write("'"+key +"'"+ ' => ' + str(dict_first2words_3rdword_counts[key])+'\n')
+            w.write("'"+key +"'"+ ' => ' + str(dict_counts[key])+'\n')
         except: 
             pass
 
